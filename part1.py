3780b65f7b064c5a736164e160e026c344d5348b
James Hocking <jameshocking542@gmail.com>
2024-03-13 17:25:16 +1100

    actual format

--- part1.py
+++ part1.py
@@ -1,7 +1,105 @@
-# get nums needs to handle more then 4   
-import functions
+def basic_get_nums_in_a_row(grid, p):
+    """
+        get_nums_in_a_row:
+            @params: 
+                grid: List[str] -> list of row stirings holding the current position
+                p: str -> player color as either 'r' or 'y'
+            @returns:
+                nums: Dict[int] -> dictionary of how many in a row 
 
-MAX_DEPTH = 2
+    """
+    nums = {"2":0, "3":0, "4":0}
+    # check rows
+    for row in grid:
+        current_in_a_row = 0
+        for i in range(len(row)):
+            if row[i] == p:
+                current_in_a_row += 1
+            else:
+                if str(current_in_a_row) in nums.keys():
+                    nums[str(current_in_a_row)] += 1
+                current_in_a_row = 0
+        if str(current_in_a_row) in nums.keys():
+            nums[str(current_in_a_row)] += 1
+        current_in_a_row = 0
+
+  # check columns
+    for i in range(len(grid[0])):
+        current_in_a_column = 0
+        for j in range(len(grid)):
+            char = grid[j][i] # iterate through each row of the column
+            if char == p:
+                current_in_a_column += 1
+            else:
+                if str(current_in_a_column) in nums.keys():
+                    nums[str(current_in_a_column)] += 1
+                current_in_a_column = 0
+        if str(current_in_a_column) in nums.keys():
+            nums[str(current_in_a_column)] += 1
+
+  # check diagonals
+    AMOUNT_OF_DIAGONALS = 12
+    i0, j0 = 5, 0
+    for _ in range(AMOUNT_OF_DIAGONALS):
+        current_in_diag = 0
+        i, j = i0, j0
+        while (i < len(grid) and j < len(grid[0])):
+            char = grid[i][j]
+            if char == p:
+                current_in_diag += 1
+            else:
+                if str(current_in_diag) in nums.keys():
+                    nums[str(current_in_diag)] += 1
+                current_in_diag = 0
+            i += 1
+            j += 1
+        if str(current_in_diag) in nums.keys():
+            nums[str(current_in_diag)] += 1
+
+        if i0 - 1 >= 0:
+            i0 -= 1
+        else:
+            j0 += 1
+    i0, j0 = 0, 0
+    for _ in range(AMOUNT_OF_DIAGONALS):
+        current_in_diag = 0
+        i, j = i0, j0
+        while (i < len(grid) and j >= 0):
+            char = grid[i][j]
+            if char == p:
+                current_in_diag += 1
+            else:
+                if str(current_in_diag) in nums.keys():
+                    
+                    nums[str(current_in_diag)] += 1
+                current_in_diag = 0
+            i += 1
+            j -= 1
+        if str(current_in_diag) in nums.keys():
+            nums[str(current_in_diag)] += 1
+        if j0 + 1 < len(grid[0]):
+            j0 += 1
+        else:
+            i0 += 1          
+    return nums
+
+  
+def score(grid, p):
+    in_a_row = basic_get_nums_in_a_row(grid, p)
+    s = 0
+    for row in grid:
+        for i in range(len(row)):
+            if row[i] == p:
+                s += 1
+    s += 10 * in_a_row["2"]
+    s += 100 * in_a_row["3"]
+    s += 1000 * in_a_row["4"]
+    return s
+
+def eval(grid):
+    return score(grid, "r") - score(grid, "y")
+  
+# get nums needs to handle more then 4   
 COLUMNS = 7
 ROWS = 6
 
@@ -15,7 +113,7 @@ class Node():
         self.color = color
 
     def set_val(self):
-        self.val = functions.eval(self.board_state)
+        self.val = eval(self.board_state)
 
     def get_children(self):
         for j in range(COLUMNS):
@@ -33,8 +131,8 @@ class Node():
             newNode = Node(new_board_state, self.depth + 1, j, new_color)
             self.children.append(newNode)        
 
-def minmax(node: Node):   
-    if node.depth == MAX_DEPTH:
+def minmax(node: Node, max_depth):   
+    if node.depth == max_depth:
         node.set_val()
         return (node.column, node.val, 1)
     
@@ -47,7 +145,7 @@ def minmax(node: Node):
         final_val = float("-inf") if finding_max else float("inf")
         final_col = 0
         for child in node.children:
-            curr_col, curr_val, n_children = minmax(child)
+            curr_col, curr_val, n_children = minmax(child, max_depth)
             counter += n_children
             if finding_max and curr_val > final_val:
                 final_val = curr_val
@@ -60,18 +158,16 @@ def minmax(node: Node):
         return (node.column, node.val, 1)
 
     return (final_col, final_val, counter)
-
-
-def main(grid, colour):
+  
+def connect_four_mm(grid, colour, depth):
+    MAX_DEPTH = depth
     grid = grid.split(",")
     grid.reverse()
     p = "r" if colour == "red" else "y"
     initial_node = Node(grid, 0, 0, p)
-    col, val, count = minmax(initial_node)
-    print(col)
-    print(count+1)
+    col, val, count = minmax(initial_node, depth)
+    return f"{col}\n{count+1}"
 
-
-main(".ryyrry,.rryry.,..y.r..,..y....,.......,.......", "red")
-
-
+if __name__ == '__main__':
+    # Example function call below, you can add your own to test the connect_four_mm function
+    connect_four_mm(".......,.......,.......,.......,.......,.......", "red", 1)
